0a014e37b685d46f0a35bfe96e76e8b589146566
AndreyR <andrew.a.rogozin@gmail.com>
2024-03-23 11:58:31 +0300

    обновлен fft для файла

--- seminar4/binary_files.py
+++ seminar4/binary_files.py
@@ -4,35 +4,44 @@ import numpy as np
 
 
 
-with open("sample-3s.wav", "rb") as file:
+with open("1kHz_44100Hz_16bit_05sec.wav", "rb") as file:
     data = file.read()
 
 sampleRate = struct.unpack_from('i', data, offset=24)[0]
 print(sampleRate)
 
 
-pureAdcData =[a for a in struct.iter_unpack("h",data[44:])]
+pureAdcData =[a[0] for a in struct.iter_unpack("h",data[45::2])]
 
 # print(pureAdcData)
 
-chartLen = 100
+chartLen = 1000
 
-time = [temp/sampleRate for temp in range(0, chartLen)]
-signal = np.cos(np.asarray(time)*2.0*np.pi*sampleRate) #pureAdcData[0:chartLen]
+# maxFreq = 20e3
+# chartLen = 
 
-plt.plot(time,signal)
-plt.grid()
-plt.xlabel('t, с')
-plt.ylabel('S')
-plt.show()
+time = [temp/(sampleRate/2) for temp in range(0, chartLen)]
+signal = pureAdcData[0:chartLen]
 
-spectr = np.fft.fftshift(np.fft.fft(signal))
+# plt.plot(time,signal)
+# plt.grid()
+# plt.xlabel('t, с')
+# plt.ylabel('S')
+# plt.show()
+
+spectr = np.fft.fft(signal)/chartLen
 
 freq = [ftemp/time[-1] for ftemp in range(0, chartLen)]
 
-plt.plot(freq,spectr)
-plt.grid()
-plt.xlabel('f, Гц')
-plt.ylabel('S')
-plt.show()
+# plt.stem(freq,np.abs(spectr))
+# plt.grid()
+# plt.xlabel('f, Гц')
+# plt.ylabel('S')
 
+
+
+fig, axs = plt.subplots(1, 2, sharey=False)
+axs[0].plot(time,signal)
+axs[1].stem(freq,np.abs(spectr))
+
+plt.show()
