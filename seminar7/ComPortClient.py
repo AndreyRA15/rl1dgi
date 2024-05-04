f4d44454b803311d49980dfe33e054b667619d48
andre <andrew.a.rogozin@gmail.com>
2024-05-04 12:41:36 +0300

    Обновлен класс клиента

--- seminar7/ComPortClient.py
+++ seminar7/ComPortClient.py
@@ -8,31 +8,39 @@ class ComPortClient():
             self.port = serial.Serial(port=portName, baudrate=baudRate)
             self.Connected = True
             print("Соединение установлено")
-        except:
+        except serial.SerialException:
             self.Connected = False
-            print("Соединение не установлено")
+            print("Невозможно установить соединение")
+        except:
+            print("Вознкла другая ошибка")
 
     def Disconnect(self):
         self.port.close()
 
     def Send(self, data):
-        if(type(data) is bytes):
-            print("Отправка сообщения", data.decode(helper.encoding))
-            self.port.write(data)
-            print("Успешно отправлено")
-        elif(type(data) is str):
-            print("Отправка сообщения", data)
-            self.port.write(data.encode(helper.encoding))
-            print("Успешно отправлено")
+        if(self.Connected):
+            if(type(data) is bytes):
+                print("Отправка сообщения", data.decode(helper.encoding))
+                self.port.write(data)
+                print("Успешно отправлено")
+            elif(type(data) is str):
+                print("Отправка сообщения", data)
+                self.port.write(data.encode(helper.encoding))
+                print("Успешно отправлено")
+            else:
+                print("Поддерживаются только типы str и bytes")
         else:
-            print("Поддерживаются только типы str и bytes")
-        
+            print("Device is not connected")
         
     def ReceiveBytes(self):
-        print("Ожидание ответа...")
-        resp = self.port.read_until()
-        print("Ответ получен:", resp.decode(helper.encoding))
-        return resp
+        if(self.Connected):
+            print("Ожидание ответа...")
+            resp = self.port.read_until()
+            print("Ответ получен:", resp.decode(helper.encoding))
+            return resp
+        else:
+            print("Device is not connected")
+            return b""
 
     def ReceiveStr(self):
         resp = self.ReceiveBytes()
